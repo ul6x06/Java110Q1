7e0d40dcab1289747216741311e247d5fe74d7e8
ul6x06 <91857915+ul6x06@users.noreply.github.com>
2021-11-08 19:48:01 +0800

    Update Class02.java

--- Class02.java
+++ Class02.java
@@ -1,23 +1,32 @@
+import java.util.Scanner;
+
 class is_prime {
+	
 	public is_prime(int a) {
 		int b = 0;
+		
 		for (int i = 1; i < a; i++) {
 			if (a % i == 0) {
 				b = b + 1;
 			}
 		}
 		if (b == 1) {
-			System.out.println("YES");
+			System.out.print(a);
+			System.out.println("     YES");
 		} else {
-			System.out.println("NO");
+			System.out.print(a);
+			System.out.println("     NO");
 		}
 	}
 }
 
 public class Class02 {
 	public static void main(String[] args) {
-		is_prime show0 = new is_prime(23);
-		is_prime show1 = new is_prime(37);
-		is_prime show2 = new is_prime(39);
+		  
+		System.out.print("Input a number:");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		is_prime show0 = new is_prime(number);
+		
 	}
 }
